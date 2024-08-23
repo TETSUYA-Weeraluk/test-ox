d20460da1f508032bdfdec45301f0b6dda9198db
Weeraluk <weeraluksop.mayawizard@gmail.com>
2024-08-23 17:43:22 +0700

    update : install packgage @react-oauth/google

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+  <GoogleOAuthProvider clientId={clientId}>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </GoogleOAuthProvider>
 );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 import Home from "../pages/Home/Home";
-import SecondLayout from "../layouts/SecondLayout";
-import PokemonDetailPage from "../pages/Home/PokemonDetailPage";
 
 const router = createBrowserRouter([
   {
@@ -12,20 +10,6 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
-      },
-      {
-        path: "pokemon/:name",
-        element: <PokemonDetailPage />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <SecondLayout />,
-    children: [
-      {
-        path: "second",
-        element: <p>Second</p>,
       },
     ],
   },
